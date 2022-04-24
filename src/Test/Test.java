6e7aa84abc7b3fc6055e545ccd1189813d2f2cfd
Nilesh <nileshgade29@yahoo.co.in>
2022-04-24 10:15:07 +0530

    This is my First Commit on 24th April 2022

---
+++ src/Test/Test.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes" )
+			
+		{
+			
+			System.out.println("Eat Something ");
+			
+		}
+		
+		else 
+		{
+			
+			System.out.println("Do work..........");
+		}
+
+	}
+
+}
